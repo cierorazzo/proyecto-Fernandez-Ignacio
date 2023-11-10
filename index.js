30e1e457b7a5908a958f21cddbc61081138d2faa
Ignacio Fernandez <nachof5cjs@gmail.com>
2023-11-10 07:12:16 -0300

    repasando

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ const PORT = process.env.PORT || 3000;
 const productRouter = require( "./routes/prodRoute" );
 const categoryRouter = require( "./routes/categoryRoute" );
 const cartRouter = require( "./routes/cartRoute" )
-const morgan = require( "morgan" );
 
 dbConnect();
 
-app.use( morgan( "dev" ) );//Cuanto demora en llegar la solicitud
 app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded( { extended:false } ))
+app.use( bodyParser.urlencoded( { extended:false } ));
 
 //llamado para interactuar con LOGIN y CARRITO DE COMPRA(gral)
 app.use( "/api/product", productRouter );
